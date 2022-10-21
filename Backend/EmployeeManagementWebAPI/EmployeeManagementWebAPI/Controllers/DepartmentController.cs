8a01a7fa614bcf92d6f2f349fe3760dc768d89f3
Tony Minseok Kim <tony.minseokkim@tectiqom.de>
2022-10-21 07:42:14 -0700

    Connection String added / Model folder added with Department.cs, Employee.cs / DepartmentController added / GetDepartment works in Postman

---
+++ Backend/EmployeeManagementWebAPI/EmployeeManagementWebAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EmployeeManagementWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            /*
+             * this is raw queries from the tutorial video,
+             * and should avoid using this.
+             * Instead, use stored procedures with parameters or
+             * entity framework to connect to database
+             */
+            string query = @"SELECT
+                                DepartmentId, DepartmentName
+                            
+                             FROM
+                                dbo.Department";
+
+            DataTable table = new DataTable();
+
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+
+            SqlDataReader myReader;
+            using (SqlConnection myCon=new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+
+                }
+            }
+
+            return new JsonResult(table);
+
+        }
+    }
+}
