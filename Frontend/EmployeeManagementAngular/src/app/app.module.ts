d7f397d5b5ebd72e1000ab49b8d9e785212a3ada
Tony Minseok Kim <tonykim.minseok@gmail.com>
2022-10-26 07:02:52 -0700

    adding APIs and completing service file.

--- Frontend/EmployeeManagementAngular/src/app/app.module.ts
+++ Frontend/EmployeeManagementAngular/src/app/app.module.ts
@@ -3,16 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentComponent } from './department/department.component';
+import { ShowDepartmentComponent } from './department/show-department/show-department.component';
+import { AddEditDepartmentComponent } from './department/add-edit-department/add-edit-department.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ShowEmployeeComponent } from './employee/show-employee/show-employee.component';
+import { AddEditEmployeeComponent } from './employee/add-edit-employee/add-edit-employee.component';
+
+/*
+  adding entry for the service file from 'ng generate service shared'
+  then add 'SharedService' in providers
+*/
+import { SharedService } from './shared.service';
+
+/*
+  adding Http Module to consume all the API methods in shared service file 
+*/
+import { HttpClientModule } from '@angular/common/http';
+
+/*
+  adding modules related to forms
+*/
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent,
+    ShowDepartmentComponent,
+    AddEditDepartmentComponent,
+    EmployeeComponent,
+    ShowEmployeeComponent,
+    AddEditEmployeeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [SharedService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ Frontend/EmployeeManagementAngular/src/app/shared.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+
+/*
+  adding HttpClient, so it can use Http modules that's connected with app.module.ts,
+  and adding Observable module for handling asynchronous requests and responses
+*/
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SharedService {
+  readonly APIUrl = 'https://localhost:44355/api';
+  readonly PhotoUrl = 'https://localhost:44355/Photos';
+
+  constructor(private http: HttpClient) {}
+
+  // Department APIs
+  getDepartmentsList(): Observable<any[]> {
+    return this.http.get<any>(this.APIUrl + '/department');
+  }
+
+  addDepartment(val: any) {
+    return this.http.post(this.APIUrl + '/department', val);
+  }
+
+  updateDepartment(val: any) {
+    return this.http.put(this.APIUrl + '/department', val);
+  }
+
+  // Employee APIs
+  getEmployeesList(): Observable<any[]> {
+    return this.http.get<any>(this.APIUrl + '/employee');
+  }
+
+  addEmployee(val: any) {
+    return this.http.post(this.APIUrl + '/employee', val);
+  }
+
+  updateEmployee(val: any) {
+    return this.http.put(this.APIUrl + '/employee', val);
+  }
+
+  // Upload photo Api
+  uploadPhoto(val: any) {
+    return this.http.post(this.APIUrl + '/employee/savefile', val);
+  }
+
+  // Get all names for departments
+  getAllDepartmentNames(): Observable<any[]> {
+    return this.http.get<any[]>(
+      this.APIUrl + '/employee/getAllDepartmentNames'
+    );
+  }
+}
