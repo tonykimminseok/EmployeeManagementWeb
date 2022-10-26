9da6246914e81b70ce6d2c0bf32696c4d1389c7d
Tony Minseok Kim <tonykim.minseok@gmail.com>
2022-10-26 07:44:28 -0700

    completing routes for frontend

--- Frontend/EmployeeManagementAngular/src/app/app-routing.module.ts
+++ Frontend/EmployeeManagementAngular/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Components
+import { DepartmentComponent } from './department/department.component';
+import { EmployeeComponent } from './employee/employee.component';
+
+const routes: Routes = [
+  { path: 'employee', component: EmployeeComponent },
+  { path: 'department', component: DepartmentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
